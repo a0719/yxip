dac535a67be24b7f798f60fdf839e69149e78b0e
a0719 <a0719@proton.me>
2025-08-15 13:52:47 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,7 +6,7 @@ import os
 # 目标URL列表
 urls = [
     'https://ip.164746.xyz', 
-    'https://cf.vvhan.com/', 
+    'https://cf.vvhan.com', 
     'https://stock.hostmonit.com/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html'
 ]
